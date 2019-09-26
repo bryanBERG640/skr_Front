cdf7709b862b634756a1a2e623c10e407d062233
xzalox <xzalox.hc@gmail.com>
2019-09-26 18:03:00 -0500

    modificaciones tablas datos

--- src/componentes/ficha.postulantes/Citas.js
+++ src/componentes/ficha.postulantes/Citas.js
@@ -1,13 +1,8 @@
 import React from "react";
-import {
-  TableHead,
-  TableBody,
-  Table,
-  TableCell,
-  TableRow
-} from "@material-ui/core";
+import { TableHead, TableBody, Table, TableCell, TableRow} from "@material-ui/core";
 import { getPostulanteC } from "../../request/request";
 import Checkbox from "@material-ui/core/Checkbox";
+import Paper from '@material-ui/core/Paper';
 
 export default class Citas extends React.Component {
   state = {
@@ -26,115 +21,40 @@ export default class Citas extends React.Component {
     const citas = this.state.resp.map(datos => {
       return datos.postulanteb.cita.map(date => {
         return (
-          <div>
-            <TableRow>
-              <TableCell>{datos.postulanteb.nombre}</TableCell>
-            </TableRow>
-          </div>
-        );
-      });
-    });
-    const citas1 = this.state.resp.map(datos => {
-      return datos.postulanteb.cita.map(date => {
-        return (
-          <div>
-            <TableRow>
-              <TableCell>{datos.postulanteb.apellido1}</TableCell>
-            </TableRow>
-          </div>
-        );
-      });
-    });
-    const citas2 = this.state.resp.map(datos => {
-      return datos.postulanteb.cita.map(date => {
-        return (
-          <div>
-            <TableRow>
-              <TableCell>{datos.postulanteb.apellido2}</TableCell>
-            </TableRow>
-          </div>
-        );
-      });
-    });
-    const citas3 = this.state.resp.map(datos => {
-      return datos.postulanteb.cita.map(date => {
-        return (
-          <div>
-            <TableRow>
-              <TableCell>{date.fecha}</TableCell>
-            </TableRow>
-          </div>
-        );
-      });
-    });
-    const citas4 = this.state.resp.map(datos => {
-      return datos.postulanteb.cita.map(date => {
-        return (
-          <div>
-            <TableRow>
-              <TableCell>{date.hora}</TableCell>
-            </TableRow>
-          </div>
-        );
-      });
-    });
-    const citas5 = this.state.resp.map(datos => {
-      return datos.postulanteb.cita.map(date => {
-        return (
-          <div>
-            <TableRow>
-              <TableCell>{date.entrevistador}</TableCell>
-            </TableRow>
-          </div>
-        );
-      });
-    });
-    const citas6 = this.state.resp.map(datos => {
-      return datos.postulanteb.cita.map(date => {
-        return (
-          <div>
-            <TableRow>
-              <TableCell>{date.observaciones}</TableCell>
-            </TableRow>
-          </div>
-        );
-      });
-    });
-    const citas7 = this.state.resp.map(datos => {
-      return datos.postulanteb.cita.map(date => {
-        return (
-          <div>
-            <TableRow>
-              <Checkbox style={{ color: "#6D107D" }} checked/>
-            </TableRow>
-          </div>
+          <TableRow>
+            <TableCell style={{ fontSize: "12px" }}>{datos.postulanteb.nombre}</TableCell>
+            <TableCell style={{ fontSize: "12px" }}>{datos.postulanteb.apellido1}</TableCell>
+            <TableCell style={{ fontSize: "12px" }}>{datos.postulanteb.apellido2}</TableCell>
+            <TableCell style={{ fontSize: "12px" }}>{date.fecha}</TableCell>
+            <TableCell style={{ fontSize: "12px" }}>{date.hora}</TableCell>
+            <TableCell style={{ fontSize: "12px" }}>{date.entrevistador}</TableCell>
+            <TableCell style={{ fontSize: "12px" }}>{date.observaciones}</TableCell>
+            <TableCell><Checkbox style={{ color: "#6D107D" }} checked /></TableCell>
+          </TableRow>
         );
       });
     });
     return (
       <div>
-        <Table>
-          <TableHead style={{ background: "#b4e5f7" }}>
-            <TableCell style={{ fontSize: "12px" }}>Nombre</TableCell>
-            <TableCell style={{ fontSize: "12px" }}>Apellido Paterno</TableCell>
-            <TableCell style={{ fontSize: "12px" }}>Apellido Materno</TableCell>
-            <TableCell style={{ fontSize: "12px" }}>Fecha</TableCell>
-            <TableCell style={{ fontSize: "12px" }}>Hora</TableCell>
-            <TableCell style={{ fontSize: "12px" }}>Entrevistador</TableCell>
-            <TableCell style={{ fontSize: "12px" }}>Observaciones</TableCell>
-            <TableCell style={{ fontSize: "12px" }}>Asistió</TableCell>
-          </TableHead>
-          <TableBody>
-            <TableCell style={{ fontSize: "10px" }}>{citas}</TableCell>
-            <TableCell style={{ fontSize: "10px" }}>{citas1}</TableCell>
-            <TableCell style={{ fontSize: "10px" }}>{citas2}</TableCell>
-            <TableCell style={{ fontSize: "10px" }}>{citas3}</TableCell>
-            <TableCell style={{ fontSize: "10px" }}>{citas4}</TableCell>
-            <TableCell style={{ fontSize: "10px" }}>{citas5}</TableCell>
-            <TableCell style={{ fontSize: "10px" }}>{citas6}</TableCell>
-            <TableCell>{citas7}</TableCell>
-          </TableBody>
-        </Table>
+        <Paper>
+          <Table>
+            <TableHead style={{ background: "#bbe5f7" }}>
+              <TableRow>
+                <TableCell align="center" style={{ fontSize: "14px" }}>Nombre</TableCell>
+                <TableCell align="center" style={{ fontSize: "14px" }}>Apellido Paterno</TableCell>
+                <TableCell align="center" style={{ fontSize: "14px" }}>Apellido Materno</TableCell>
+                <TableCell align="center" style={{ fontSize: "14px" }}>Fecha</TableCell>
+                <TableCell align="center" style={{ fontSize: "14px" }}>Hora</TableCell>
+                <TableCell align="center" style={{ fontSize: "14px" }}>Entrevistador</TableCell>
+                <TableCell align="center" style={{ fontSize: "14px" }}>Observaciones</TableCell>
+                <TableCell align="center" style={{ fontSize: "14px" }}>Asistió</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {citas}
+            </TableBody>
+          </Table>
+        </Paper>
       </div>
     );
   }
